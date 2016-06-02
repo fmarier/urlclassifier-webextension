27908d656c44f405453b0eadb48fa92f2bcda81f
Francois Marier <francois@mozilla.com>
2016-06-02 16:45:11 -0700

    Fetch the filters from a remote URL
    Restricted to HTTPS in the spirit of https://w3c.github.io/webappsec-secure-contexts/#new

--- background.js
+++ background.js
@@ -1,5 +1,15 @@
 function handleClick() {
-  browser.urlClassifier.setFilters('||fmarier.org/img/francois_marier.jpg');
+  var url = 'https://people.mozilla.org/~fmarier/block.txt';
+  var myRequest = new Request(url);
+  fetch(myRequest).then(function(response) {
+    if (response.ok) {
+      response.text().then(function (text) {
+        browser.urlClassifier.setFilters(text);
+      });
+    } else {
+      console.log('Failed to fetch filters at ' + url + ': ' + response.statusText);
+    }
+  });
 }
 
 browser.browserAction.onClicked.addListener(handleClick);
