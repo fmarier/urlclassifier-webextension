8ffb0847141f44a0c99a26e96a41aa22c003339c
Francois Marier <francois@mozilla.com>
2016-06-02 18:10:09 -0700

    Move the hard-coded URL into a local config
    There is still no UI to change it though.

--- background.js
+++ background.js
@@ -1,15 +1,42 @@
-function handleClick() {
-  var url = 'https://people.mozilla.org/~fmarier/block.txt';
-  var myRequest = new Request(url);
-  fetch(myRequest).then(function(response) {
-    if (response.ok) {
-      response.text().then(function (text) {
-        browser.urlClassifier.setFilters(text);
-      });
+function getFiltersUrl(callback) {
+  browser.storage.local.get('config', function (keys) {
+    console.log("config=" + JSON.stringify(keys));
+    if (!keys || !keys.config || !keys.config.filtersUrl) {
+      callback("");
     } else {
-      console.log('Failed to fetch filters at ' + url + ': ' + response.statusText);
+      callback(keys.config.filtersUrl);
     }
   });
 }
 
-browser.browserAction.onClicked.addListener(handleClick);
+function saveConfig(config, callback) {
+  browser.storage.local.set({config: config}, callback);
+}
+
+function handleClick() {
+  getFiltersUrl(function (url) {
+    console.log('Fetching filters at ' + url + '...');
+    var myRequest = new Request(url);
+    fetch(myRequest).then(function(response) {
+      if (response.ok) {
+        response.text().then(function (text) {
+          browser.urlClassifier.setFilters(text);
+        });
+      } else {
+        console.log('Failed to fetch filters at ' + url + ': ' + response.statusText);
+      }
+    });
+  });
+}
+
+function init() {
+  browser.browserAction.onClicked.addListener(handleClick);
+}
+
+getFiltersUrl(function (url) {
+  if (!url) {
+    saveConfig({filtersUrl: 'https://people.mozilla.org/~fmarier/block.txt'}, init);
+  } else {
+    init();
+  }
+});
